e0e25978e2861f94ebe21eb26d6ced5095bb7a3c
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-05-18 07:17:51 +0530

    Update metadata and app icon for mobile

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,25 +40,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: "Kenny's Blog",
+        short_name: `Kenny`,
+        description: `Kenny's personal blog`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
         display: `standalone`,
-        icons: [
-          {
-            src: `/android-chrome-192x192.png`,
-            sizes: `192x192`,
-            type: `image/png`,
-          },
-          {
-            src: `/android-chrome-512x512.png`,
-            sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
+        icon: "static/kenny.png",
+        include_favicon: false,
       },
     },
     `gatsby-plugin-offline`,
